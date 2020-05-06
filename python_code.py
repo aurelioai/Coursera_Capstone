955a520b4cff10ec6f2faae77ba55e31f697adb8
Aurelio AI <aurelio@Mac-mini-de-Aurelio.local>
2020-05-06 13:13:14 -0500

    Adding solution to problem 1 of final project.

--- python_code.py
+++ python_code.py
@@ -1,4 +1,25 @@
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
+import markdown
+import math
+print
 print("Hello Github!")
 print("Hello Capstone Project Course!")
+html = markdown.markdown("<b>A ver qué onda</b>")
+print(html)
+x = np.linspace(0, 10, 100)
+
+# Plot the data
+plt.plot(x, x**2, label='linear')
+
+# Add a legend
+plt.legend()
+
+# Show the plot
+plt.show()
+
+print(0.3)
+print(math.fsum([0.1,0.1,0.1]))
+print(0.1+0.1+0.1)
